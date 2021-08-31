6b928008decd49b939777e718878e536fd6ffce6
Manu Lange <Manu.Lange@plantandfood.co.nz>
2021-08-31 14:46:39 +1200

    run black

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,15 +1,19 @@
 import pytest
 from click.testing import CliRunner
 from pytomicropy.core import main
+
 runner = CliRunner()
+
 
 def test_cli(tmp_path):
     indir = tmp_path / "in"
     outdir = tmp_path / "out"
     indir.mkdir()
-    (indir / "main.py").write_text("""
+    (indir / "main.py").write_text(
+        """
 print(f"Hello from{__file__}")
-""")
+"""
+    )
     response = runner.invoke(main, ["--input", indir, "--output", outdir])
     assert not response.exception
     assert outdir.exists and outdir.is_dir()
