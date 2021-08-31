a5f5115b167af1be039a981f12e53a92ccaed90b
Manu Lange <Manu.Lange@plantandfood.co.nz>
2021-08-31 14:44:38 +1200

    adds basic test

---
+++ tests/test_basic.py
@@ -0,0 +1,17 @@
+import pytest
+from click.testing import CliRunner
+from pytomicropy.core import main
+runner = CliRunner()
+
+def test_cli(tmp_path):
+    indir = tmp_path / "in"
+    outdir = tmp_path / "out"
+    indir.mkdir()
+    (indir / "main.py").write_text("""
+print(f"Hello from{__file__}")
+""")
+    response = runner.invoke(main, ["--input", indir, "--output", outdir])
+    assert not response.exception
+    assert outdir.exists and outdir.is_dir()
+    # Ensure f-strings are replaced
+    assert (outdir / "main.py").read_text() == "print('Hello from{}'.format(__file__))"
